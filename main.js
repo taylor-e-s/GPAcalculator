d6571c77c6bc9620b5ab8ca8bd4e4b8e685565f9
taylor-e-s <202455@pennmanor.net>
2021-04-19 09:05:18 -0400

    define variables and start seperating classes

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
+//reading the GPA file and splitting it by line
 var fs = require('fs');
-var text = fs.readFileSync("major.gpa.txt", 'utf-8');
-let array = text.split(' ')
-console.log(array)
+var grades = fs.readFileSync("majorgpa.txt", 'utf-8').split('\n');
+let i = 0
+let classes = grades.length
+let credits = 0
+let currentClass =grades[0]
+// getting each class individually
+while(i< classes){
+    if(currentClass.startsWith('C')){
+        console.log('CP')
+    //if the grade is this, add this else if this, else if this, else if this, else this
+    }else if(currentClass.startsWith('A')){
+        console.log('AP')
+    }else{
+        console.log('honors')
+    }
+    i++
+    currentClass = grades[i]
+}
+//printing final GPA
+//console.log(credits/classes)
