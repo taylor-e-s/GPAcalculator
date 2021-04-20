fc516048e76888b43d051ca6ccc68901bc1fac89
taylor-e-s <202455@pennmanor.net>
2021-04-20 08:41:53 -0400

    complete base program

--- main.js
+++ main.js
@@ -1,22 +1,65 @@
 //reading the GPA file and splitting it by line
 var fs = require('fs');
 var grades = fs.readFileSync("majorgpa.txt", 'utf-8').split('\n');
+//defining variables for while loop
 let i = 0
 let classes = grades.length
-let credits = 0
-let currentClass =grades[0]
-// getting each class individually
+let credits = 0.0
+let currentClass = grades[0]
+//looks through every class and checks level and grade
 while(i< classes){
     if(currentClass.startsWith('C')){
-        console.log('CP')
-    //if the grade is this, add this else if this, else if this, else if this, else this
-    }else if(currentClass.startsWith('A')){
-        console.log('AP')
+        if(currentClass.charAt(currentClass.length -1) === 'A'){
+            credits+=4.0
+        }else if(currentClass.charAt(currentClass.length -1) === 'B'){
+            credits+=3.0
+        }else if(currentClass.charAt(currentClass.length -1) === 'C'){
+            credits+=2.0
+        }else if(currentClass.charAt(currentClass.length -1) === 'D'){
+            credits+=1.0
+        }else{
+            credits+=0.0
+        }
+    }else if(currentClass.startsWith('H')){
+        if(currentClass.charAt(currentClass.length -1) === 'A'){
+            credits+=4.5
+        }else if(currentClass.charAt(currentClass.length -1) === 'B'){
+            credits+=3.5
+        }else if(currentClass.charAt(currentClass.length -1) === 'C'){
+            credits+=2.5
+        }else if(currentClass.charAt(currentClass.length -1) === 'D'){
+            credits+=1.5
+        }else{
+            credits+=0.0
+        }
+    }else if(currentClass.startsWith('AP+')){
+        if(currentClass.charAt(currentClass.length -1) === 'A'){
+            credits+=5.5
+        }else if(currentClass.charAt(currentClass.length -1) === 'B'){
+            credits+=4.5
+        }else if(currentClass.charAt(currentClass.length -1) === 'C'){
+            credits+=3.5
+        }else if(currentClass.charAt(currentClass.length -1) === 'D'){
+            credits+=2.5
+        }else{
+            credits+=0.0
+        }
     }else{
-        console.log('honors')
+        if(currentClass.charAt(currentClass.length -1) === 'A'){
+            credits+=5.0
+        }else if(currentClass.charAt(currentClass.length -1) === 'B'){
+            credits+=4.0
+        }else if(currentClass.charAt(currentClass.length -1) === 'C'){
+            credits+=3.0
+        }else if(currentClass.charAt(currentClass.length -1) === 'D'){
+            credits+=2.0
+        }else{
+            credits+=0.0
+        }
     }
-    i++
-    currentClass = grades[i]
+//adds to one to continue loop and defines new current class
+i += 1
+currentClass = grades[i]
 }
 //printing final GPA
-//console.log(credits/classes)
+console.log(credits/classes)
